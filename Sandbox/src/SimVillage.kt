20806c3d6698b2c211132c5e32fb274369f9c7ae
LuiX <luis.a.vivero@gmail.com>
2019-03-10 13:50:07 -0700

    Using type inference for greetingFunction

--- Sandbox/src/SimVillage.kt
+++ Sandbox/src/SimVillage.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2019
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, Major $playerName! (copyright $currentYear)"
