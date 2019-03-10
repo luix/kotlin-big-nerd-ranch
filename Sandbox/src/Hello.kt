fdbc53fbe031400756c3d59719db68d496971cc3
LuiX <luis.a.vivero@gmail.com>
2019-03-10 13:39:25 -0700

    Shorthand for when a lambda is passed as the last argument into a function.

--- Sandbox/src/Hello.kt
+++ Sandbox/src/Hello.kt
@@ -2,9 +2,7 @@ fun main(args: Array<String>) {
     println("Hello, world!")
 
     // Chapter 5. Anonymous Functions and the Function Type
-    val numLetters = "Mississippi".count({ letter ->
-        letter == 's'
-    })
+    val numLetters = "Mississippi".count { it == 's' }
     println(numLetters)
 }
 
