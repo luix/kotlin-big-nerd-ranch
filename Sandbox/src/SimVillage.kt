751c8f3925b8be6855efb942caf18e3f80980679
LuiX <luis.a.vivero@gmail.com>
2019-03-10 21:05:40 -0700

    Defining the printConstructionCost function

--- Sandbox/src/SimVillage.kt
+++ Sandbox/src/SimVillage.kt
@@ -10,3 +10,8 @@ fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String)
     val numBuildings = (1..3).shuffled().last()     // Randomly selects 1, 2 or 3
     println(greetingFunction(playerName, numBuildings))
 }
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("construction cost: ${cost * numBuildings}")
+}
