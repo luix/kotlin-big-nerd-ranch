3ff4ad289a89c06113eeccd46b76652479e46dce
LuiX <luis.a.vivero@gmail.com>
2019-03-12 19:11:32 -0700

    Using let and the null coalescing operators.

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -2,12 +2,9 @@ fun main(args: Array<String>) {
     var beverage = readLine()
     // beverage = null
 
-    if (beverage != null) {
+    beverage?.let {
         beverage.capitalize()
-    } else {
-        println("I can't do that without crashing - beverage was null!")
-    }
+    } ?: "Butter Ale"
 
-    val beverageServed: String = beverage ?: "Butter Ale"
-    println(beverageServed)
+    println(beverage)
 }
