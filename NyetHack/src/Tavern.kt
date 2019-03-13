2ad39465f4ba9f8fe100452fa6a51d2edd143f74
LuiX <luis.a.vivero@gmail.com>
2019-03-12 18:44:20 -0700

    Using the non-null assertion operator, more often called the double-bang operator.

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,11 +1,5 @@
 fun main(args: Array<String>) {
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "Buttered Ale"
-        }
-    }
+    var beverage = readLine()!!.capitalize()
     // beverage = null
 
     println(beverage)
