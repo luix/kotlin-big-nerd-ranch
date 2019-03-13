aced3388bb8f3447e8eda5fa717db1c90f70139a
LuiX <luis.a.vivero@gmail.com>
2019-03-12 20:02:53 -0700

    Adding sword juggling logic

---
+++ NyetHack/src/SwordJuggler.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var swordsJuggling: Int? = null
+    val isJugglingProficient = (1..3).shuffled().last() == 3
+    if (isJugglingProficient) {
+        swordsJuggling = 2
+    }
+
+    println("You juggle $swordsJuggling swords!")
+}
