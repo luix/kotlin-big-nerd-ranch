754874c5ae84fce49da6218198928677c4c59cba
LuiX <luis.a.vivero@gmail.com>
2019-03-12 18:54:32 -0700

    Using != null for null checking.

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,6 +1,12 @@
 fun main(args: Array<String>) {
-    var beverage = readLine()!!.capitalize()
+    var beverage = readLine()
     // beverage = null
+
+    if (beverage != null) {
+        beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage was null!")
+    }
 
     println(beverage)
 }
