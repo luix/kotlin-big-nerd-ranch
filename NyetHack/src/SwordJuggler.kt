4d15b4144ffc61439ae7f738e4dd526576374aa6
LuiX <luis.a.vivero@gmail.com>
2019-03-14 21:15:48 -0700

    Added more preconditions like require, requireNotNull and error

--- NyetHack/src/SwordJuggler.kt
+++ NyetHack/src/SwordJuggler.kt
@@ -3,6 +3,7 @@ fun main(args: Array<String>) {
     val isJugglingProficient = (1..3).shuffled().last() == 3
     if (isJugglingProficient) {
         swordsJuggling = 2
+        juggleSwords(swordsJuggling)
     }
 
     try {
@@ -17,6 +18,12 @@ fun main(args: Array<String>) {
 
 fun proficiencyCheck(swordsJuggling: Int?) {
     checkNotNull(swordsJuggling) { "Player cannot juggle swords" }
+    requireNotNull(swordsJuggling) { "Require not null!" }
+    error { " Aca hubo algun error " }
+}
+
+fun juggleSwords(swordsJuggling: Int) {
+    require(swordsJuggling >= 3) { "Juggle at least 3 swords to be exciting." }
 }
 
 class UnskilledSwordJugglerException() :
