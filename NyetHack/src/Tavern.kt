f5f0d262cc604445c7dd13dd2e52cc32eb431327
LuiX <luis.a.vivero@gmail.com>
2019-03-14 21:56:02 -0700

    Passing tavern data to placeOrder

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,10 +1,10 @@
 const val TAVERN_NAME = "Taernyl's Folly"
 
 fun main(args: Array<String>) {
-    placeOrder()
+    placeOrder("shandy,Dragon's Breath,5.91")
 }
 
-private fun placeOrder() {
+private fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order.")
