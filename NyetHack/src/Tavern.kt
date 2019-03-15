17ddb42c47bc80a07ab18aa09956dc60150bfbbe
LuiX <luis.a.vivero@gmail.com>
2019-03-14 21:59:57 -0700

    Splitting the menu data

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -8,4 +8,11 @@ private fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order.")
+
+    val data = menuData.split(',')
+    val type = data[0]
+    val name = data[1]
+    val price = data[2]
+    val message = "Madrigal buys a $name ($type) for $price pesos."
+    println(message)
 }
