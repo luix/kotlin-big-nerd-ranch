d9d6793a78f351b73e95a857d9b0987ff72a8e31
LuiX <luis.a.vivero@gmail.com>
2019-03-14 21:40:01 -0700

    Extracting the tavern master's name using indexOf and substring.

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,4 +1,11 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main(args: Array<String>) {
-    var beverage = readLine()?.capitalize().plus(", large")
-    println(beverage)
+    placeOrder()
+}
+
+private fun placeOrder() {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
 }
