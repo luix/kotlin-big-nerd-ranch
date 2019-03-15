5251edf6718733c60c8807c2d054049a8f578d34
LuiX <luis.a.vivero@gmail.com>
2019-03-14 22:06:27 -0700

    Destructing the menu data

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -9,10 +9,7 @@ private fun placeOrder(menuData: String) {
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("Madrigal speaks with $tavernMaster about their order.")
 
-    val data = menuData.split(',')
-    val type = data[0]
-    val name = data[1]
-    val price = data[2]
-    val message = "Madrigal buys a $name ($type) for $price pesos."
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for \$$price pesos."
     println(message)
 }
