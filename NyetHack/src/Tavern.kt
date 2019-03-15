eb589d4d28a43fe176786dd55159231c50c4adfd
LuiX <luis.a.vivero@gmail.com>
2019-03-14 22:42:19 -0700

    Defining the toDragonSpeak function

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -12,4 +12,19 @@ private fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "Madrigal buys a $name ($type) for \$$price pesos."
     println(message)
+
+    val phrase = "Ah, delicious $name!"
+    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
 }
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
