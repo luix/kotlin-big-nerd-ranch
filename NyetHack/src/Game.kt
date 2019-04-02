6ee97ebd39c67989adc9bacacc080f399a4d713e
LuiX <luis.a.vivero@gmail.com>
2019-04-02 09:37:31 -0700

    Adding properties to Player

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    
+
     val player = Player()
     player.castFireball(5)
 
--- NyetHack/src/Player.kt
+++ NyetHack/src/Player.kt
@@ -5,6 +5,10 @@ class Player {
             field = value.trim()
         }
 
+    var healthPoints = 89
+    var isBlessed = true
+    var isInmortal = false
+
     fun castFireball(numFireballs: Int = 2) =
             println("A glass of Fireball springs into existence. (x$numFireballs)")
 }
