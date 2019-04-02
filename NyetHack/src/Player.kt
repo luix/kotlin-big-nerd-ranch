0dbdd965e4e1a18c59dab32128ef24f7c4620df0
LuiX <luis.a.vivero@gmail.com>
2019-04-02 09:44:34 -0700

    Adding class functions to Player

--- NyetHack/src/Player.kt
+++ NyetHack/src/Player.kt
@@ -9,6 +9,27 @@ class Player {
     var isBlessed = true
     private var isInmortal = false
 
+
+    private fun auraColor_(isBlessed: Boolean,
+                           healthPoints: Int,
+                           isInmortal: Boolean): String {
+        val auraVisible = isBlessed && healthPoints > 50 || isInmortal
+        val auraColor = if (auraVisible) "GREEN" else "NONE"
+        return auraColor
+    }
+
+    private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+
     fun castFireball(numFireballs: Int = 2) =
             println("A glass of Fireball springs into existence. (x$numFireballs)")
 }
