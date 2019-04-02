01a60a160dcc15040df559a4672ae015f5a05647
LuiX <luis.a.vivero@gmail.com>
2019-04-02 09:10:49 -0700

    Defining a custom setter

--- NyetHack/src/Player.kt
+++ NyetHack/src/Player.kt
@@ -1,6 +1,9 @@
 class Player {
     val name = "madrigal"
         get() = field.capitalize()
+        set(value) {
+            field = value.trim()
+        }
 
     fun castFireball(numFireballs: Int = 2) =
             println("A glass of Fireball springs into existence. (x$numFireballs)")
