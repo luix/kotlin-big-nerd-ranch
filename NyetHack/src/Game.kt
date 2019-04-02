a61cc57189101b841040d526341ece2cd7f81d83
LuiX <luis.a.vivero@gmail.com>
2019-04-02 09:43:00 -0700

    Removing functions from main

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -32,32 +32,10 @@ private fun printPlayerStatus(auraColor: String,
     println("$name $healthStatus")
 }
 
-private fun auraColor_(isBlessed: Boolean,
-                      healthPoints: Int,
-                      isInmortal: Boolean): String {
-    val auraVisible = isBlessed && healthPoints > 50 || isInmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}
-
 // Challenge: Single-Expression Functions
 // Can you convert auraColor to use a single-expression function syntax?
 private fun auraColor(isBlessed: Boolean, healthPoints: Int, isInmortal: Boolean) =
     if (isBlessed && healthPoints > 50 || isInmortal) "green" else "none"
-
-
-// favor using single-expression function syntax when possible to make the code more concise.
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
-    100 -> "is in excellent condition!"
-    in 90..99 -> "has few scratches."
-    in 75..89 -> if (isBlessed) {
-        "has some minor wounds but is healing quite quickly!"
-    } else {
-        "has some minor wounds."
-    }
-    in 15..74 -> "looks pretty hurt."
-    else -> "is in awful condition!"
-}
 
 // Challenge: Fireball Inerbation Level
 private fun inerbationLevel(fireballs: Int) =
