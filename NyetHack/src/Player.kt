4fb7ef6051cc0bd80e8ea7d241f85e825df9433b
LuiX <luis.a.vivero@gmail.com>
2019-04-02 09:08:20 -0700

    Definig a custom getter

--- NyetHack/src/Player.kt
+++ NyetHack/src/Player.kt
@@ -1,5 +1,6 @@
 class Player {
     val name = "madrigal"
+        get() = field.capitalize()
 
     fun castFireball(numFireballs: Int = 2) =
             println("A glass of Fireball springs into existence. (x$numFireballs)")
