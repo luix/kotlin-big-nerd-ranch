8bd892c8ef9371bf4b6a66ac5f9cb9b2d84ee311
LuiX <luis.a.vivero@gmail.com>
2019-06-17 22:25:50 -0700

    Using the hometown property

--- NyetHack/src/com/bignerdranch/nyethack/Player.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Player.kt
@@ -7,7 +7,7 @@ class Player(_name: String,
              val isBlessed: Boolean,
              private val isImmortal: Boolean) {
     var name = _name
-        get() = field.capitalize()
+        get() = "${field.capitalize()} of $hometown"
         private set(value) {
             field = value.trim()
         }
