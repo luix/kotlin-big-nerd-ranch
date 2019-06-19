2bde8e4b7802afc0a7a284f2b8fe92c4b381d66f
LuiX <luis.a.vivero@gmail.com>
2019-06-18 20:43:52 -0700

    Declaring a protected property

--- NyetHack/src/com/bignerdranch/nyethack/Room.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Room.kt
@@ -1,7 +1,10 @@
 package com.bignerdranch.nyethack
 
 open class Room(val name: String) {
-    fun description() = "Room: $name"
+    protected open val dangerLevel = 5
+
+    fun description() = "Room: $name\n" +
+            "Danger level: $dangerLevel"
 
     open fun load() = "Nothing much to see here..."
 }
