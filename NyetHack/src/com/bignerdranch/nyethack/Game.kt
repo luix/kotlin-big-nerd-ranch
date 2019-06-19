41635c3036d506d818ec3516fc9824d20005a43b
LuiX <luis.a.vivero@gmail.com>
2019-06-18 20:52:37 -0700

    Calling subclass function implementation

--- NyetHack/src/com/bignerdranch/nyethack/Game.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Game.kt
@@ -5,7 +5,7 @@ fun main(args: Array<String>) {
     val player = Player("Madrigal")
     player.castFireball(5)
 
-    var currentRoom = Room("Foyer")
+    var currentRoom: Room = TownSquare()
     println(currentRoom.description())
     println(currentRoom.load())
 
