571763ed05cab953a081f3e031dd4875cb8dbd83
LuiX <luis.a.vivero@gmail.com>
2019-06-18 20:39:47 -0700

    Declaring the TownSquare class

--- NyetHack/src/com/bignerdranch/nyethack/Room.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Room.kt
@@ -6,4 +6,6 @@ open class Room(val name: String) {
     fun load() = "Nothing much to see here..."
 }
 
-class TownSquare : Room("Town Square")
+class TownSquare : Room("Town Square") {
+    override fun load() = "The villagers rally and cheer as you enter!"
+}
