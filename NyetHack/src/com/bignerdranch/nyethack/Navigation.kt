8477a8c32ff9299808b7aa5bdb89457ef5159ad7
LuiX <luis.a.vivero@gmail.com>
2019-06-20 21:08:51 -0700

    Defining an enum constructor

--- NyetHack/src/com/bignerdranch/nyethack/Navigation.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Navigation.kt
@@ -1,10 +1,10 @@
 package com.bignerdranch.nyethack
 
-enum class Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
+enum class Direction(private val coordinate: Coordinate) {
+    NORTH(Coordinate(0, -1)),
+    EAST(Coordinate(1, 0)),
+    SOUTH(Coordinate(0, 1)),
+    WEST(Coordinate(-1, 0))
 }
 
 data class Coordinate(val x: Int, val y: Int) {
