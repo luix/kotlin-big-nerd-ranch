1f44ccb6cbe7d7931ade20a2f5f31d2302d14fec
LuiX <luis.a.vivero@gmail.com>
2019-06-20 21:11:23 -0700

    Defining a function in an enum

--- NyetHack/src/com/bignerdranch/nyethack/Navigation.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Navigation.kt
@@ -4,7 +4,11 @@ enum class Direction(private val coordinate: Coordinate) {
     NORTH(Coordinate(0, -1)),
     EAST(Coordinate(1, 0)),
     SOUTH(Coordinate(0, 1)),
-    WEST(Coordinate(-1, 0))
+    WEST(Coordinate(-1, 0));
+
+    fun updateCoordinate(playerCoordinate: Coordinate) =
+            Coordinate(playerCoordinate.x + coordinate.x,
+                    playerCoordinate.y + coordinate.y)
 }
 
 data class Coordinate(val x: Int, val y: Int) {
