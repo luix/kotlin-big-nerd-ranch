006ac62231c78bb1152fd34c02735d18e477c07c
LuiX <luis.a.vivero@gmail.com>
2019-06-27 16:46:49 -0700

    Dependant jobs in action with coroutines

--- NyetHack/src/com/bignerdranch/nyethack/Main.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Main.kt
@@ -1,9 +1,35 @@
 package com.bignerdranch.nyethack
 
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 fun main() {
+    coroutineWorkers()
+
+    playPingPong()
+}
+
+
+fun playPingPong() {
+    val job1 = GlobalScope.launch(start = CoroutineStart.LAZY) {
+        delay(200)
+        println("Pong")
+        delay(200)
+    }
+    GlobalScope.launch() {
+        delay(200)
+        println("Ping")
+        job1.join()
+        println("Ping")
+        delay(200)
+    }
+    Thread.sleep(1000)
+}
+
+
+fun coroutineWorkers() {
     (1..10000).forEach {
         GlobalScope.launch {
             val threadName = Thread.currentThread().name
