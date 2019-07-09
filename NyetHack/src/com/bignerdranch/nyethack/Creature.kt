ce076e039ae8989de45d435923b9f650947da5c7
LuiX <luis.a.vivero@gmail.com>
2019-07-08 22:20:47 -0700

    Implementing properties in the subclass of an abstract class

--- NyetHack/src/com/bignerdranch/nyethack/Creature.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Creature.kt
@@ -29,4 +29,6 @@ class Goblin(name: String = "Goblin",
              description: String = "A nasty-looking goblin",
              healthPoints: Int = 30) : Monster(name, description, healthPoints) {
 
+    override val diceCount = 2
+    override val diceSides = 8
 }
