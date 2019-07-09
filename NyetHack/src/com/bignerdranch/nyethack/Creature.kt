e0478e3370ace2555e80d5035a75080d81742aee
LuiX <luis.a.vivero@gmail.com>
2019-07-08 22:10:01 -0700

    Defining a default getter implementation

--- NyetHack/src/com/bignerdranch/nyethack/Creature.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Creature.kt
@@ -1,10 +1,15 @@
 package com.bignerdranch.nyethack
 
+import java.util.*
+
 interface Fightable {
     var healthPoints: Int
     val diceCount: Int
     val diceSides: Int
     val damageRoll: Int
+        get() = (0 until diceCount).map {
+            Random().nextInt(diceSides + 1)
+        }.sum()
 
     fun attack(opponent: Fightable): Int
 }
