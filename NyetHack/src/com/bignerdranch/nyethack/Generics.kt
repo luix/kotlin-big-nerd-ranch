0530d896ccf0a0b02554638cb936b6a9f6657b8d
LuiX <luis.a.vivero@gmail.com>
2019-07-09 18:44:03 -0700

    Defining loot boxes

--- NyetHack/src/com/bignerdranch/nyethack/Generics.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Generics.kt
@@ -3,3 +3,13 @@ package com.bignerdranch.nyethack
 class LootBox<T>(item: T) {
     private var loot: T = item
 }
+
+class Fedora(val name: String, val value: Int)
+
+class Coin(val value: Int)
+
+fun main() {
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora", 15))
+    val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+}
+
