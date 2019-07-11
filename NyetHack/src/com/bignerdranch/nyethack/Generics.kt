26d1b79598853bdf985701aa16c07d5005ca10cc
LuiX <luis.a.vivero@gmail.com>
2019-07-10 21:59:25 -0700

    Using get

--- NyetHack/src/com/bignerdranch/nyethack/Generics.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Generics.kt
@@ -5,7 +5,7 @@ class LootBox<T : Loot>(vararg item: T) {
     private var loot: Array<out T> = item
 
     operator fun get(index: Int): T? = loot[index].takeIf { open }
-    
+
     fun fetch(item: Int): T? {
         return loot[item].takeIf { open }
     }
@@ -35,5 +35,8 @@ fun main(args: Array<String>) {
         Coin(it.value * 3)
     }
     coin?.let { println(it.value) }
+
+    val fedora = lootBoxOne[1]
+    fedora?.let { println(it.name) }
 }
 
