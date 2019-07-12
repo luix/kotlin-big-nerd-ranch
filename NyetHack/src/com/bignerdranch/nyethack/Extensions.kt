cce6fc4ae6823d5e75feb5f36cd19e3a94aad6cd
LuiX <luis.a.vivero@gmail.com>
2019-07-11 22:11:25 -0700

    Calling the new extension on a String receiver instance

--- NyetHack/src/com/bignerdranch/nyethack/Extensions.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Extensions.kt
@@ -2,3 +2,6 @@ package com.bignerdranch.nyethack
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
+fun main(args: Array<String>) {
+    println("Madrigal has left the building".addEnthusiasm(2))
+}
