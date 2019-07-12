cc7b0a1a3071c99f146b55e490a6b4b6879f5594
LuiX <luis.a.vivero@gmail.com>
2019-07-11 22:14:38 -0700

    Extending Any

--- NyetHack/src/com/bignerdranch/nyethack/Extensions.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Extensions.kt
@@ -2,6 +2,8 @@ package com.bignerdranch.nyethack
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
+fun Any.easyPrint() = println(this)
+
 fun main(args: Array<String>) {
-    println("Madrigal has left the building".addEnthusiasm(2))
+    "Madrigal has left the building".addEnthusiasm(2).easyPrint()
 }
