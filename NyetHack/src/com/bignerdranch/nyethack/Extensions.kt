3065450c176eb3a77d0e04893a37aa68f4dd1142
LuiX <luis.a.vivero@gmail.com>
2019-07-11 22:25:15 -0700

    Making easyPrint generic

--- NyetHack/src/com/bignerdranch/nyethack/Extensions.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Extensions.kt
@@ -2,7 +2,7 @@ package com.bignerdranch.nyethack
 
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
-fun Any.easyPrint(): Any {
+fun <T> T.easyPrint(): T {
     println(this)
     return this
 }
