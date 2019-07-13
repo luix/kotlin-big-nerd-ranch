246acdfa7854c9f0788059e0ccfb479e11debfa4
LuiX <luis.a.vivero@gmail.com>
2019-07-12 22:17:43 -0700

    Adding a private random extension

--- NyetHack/src/com/bignerdranch/nyethack/Tavern.kt
+++ NyetHack/src/com/bignerdranch/nyethack/Tavern.kt
@@ -15,6 +15,8 @@ val menuList = File("data/tavern-menu-items.txt")
         .split("\n")
 val patronGold = mutableMapOf<String, Double>()
 
+private fun <T> Iterable<T>.random(): T = this.shuffled().first()
+
 fun main(args: Array<String>) {
     if (patronList.contains("Eli")) {
         println("The tavern master says: Eli's in the back playing cards.")
