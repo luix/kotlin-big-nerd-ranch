79fdcc1c433ba7d5ad635b70c67819673fb51e29
LuiX <luis.a.vivero@gmail.com>
2019-07-13 23:16:53 -0700

    Printing the friendship level

--- NyetHack/src/com/bignerdranch/interop/Hero.kt
+++ NyetHack/src/com/bignerdranch/interop/Hero.kt
@@ -3,4 +3,7 @@ package com.bignerdranch.interop
 fun main(args: Array<String>) {
     val adversary = Jhava()
     println(adversary.utterGreeting())
+
+    val friendshipLevel = adversary.determineFriendshipLevel()
+    println(friendshipLevel.toLowerCase())
 }
