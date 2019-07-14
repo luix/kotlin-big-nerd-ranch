892a394f211ed61a2f7b57b2e0fc8dccd8ab15bc
LuiX <luis.a.vivero@gmail.com>
2019-07-13 23:17:58 -0700

    Specifying that a return value will possibly be null

--- NyetHack/src/com/bignerdranch/interop/Jhava.java
+++ NyetHack/src/com/bignerdranch/interop/Jhava.java
@@ -1,10 +1,13 @@
 package com.bignerdranch.interop;
 
+import com.sun.istack.internal.Nullable;
+
 public class Jhava {
     public String utterGreeting() {
         return "BLARGH";
     }
 
+    @Nullable
     public String determineFriendshipLevel() {
         return null;
     }
