d2d52c3d4a1fa254a1f637779b703d540ab9bc20
LuiX <luis.a.vivero@gmail.com>
2019-07-13 21:55:25 -0700

    Before and after mapping: same number of items, different types

--- NyetHack/src/com/bignerdranch/FunctionalPlayground.kt
+++ NyetHack/src/com/bignerdranch/FunctionalPlayground.kt
@@ -2,13 +2,23 @@ package com.bignerdranch
 
 
 fun main(args: Array<String>) {
+    val tenDollarWords = listOf("auspicious", "avuncular", "obviate")
+    val tenDollarWordLenghts = tenDollarWords.map { it.length }
+    println(tenDollarWordLenghts)
+    // [10, 9, 7]
+    println(tenDollarWords.size)
+    // 3
+    println(tenDollarWordLenghts.size)
+    // 3
+}
 
+
+// Transforms category of functions
+fun aTailOfBabiesAnimals() {
     val animals = listOf("zebra", "giraffe", "elephant", "rat")
     val babies = animals
             .map{ animal -> "A baby $animal" }
             .map{ baby -> "$baby, with the cutest little tail ever!"}
     println(babies)
-
     println(animals)
-
 }
