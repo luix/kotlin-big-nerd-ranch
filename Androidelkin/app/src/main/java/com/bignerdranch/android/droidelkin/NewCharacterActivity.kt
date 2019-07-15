a60e00b99e624a2c781761a9a2b1b4eb9ab5ea86
LuiX <luis.a.vivero@gmail.com>
2019-07-14 22:50:09 -0700

    Renamed MainActivity to NewCharacterActivity

--- Androidelkin/app/src/main/java/com/bignerdranch/android/droidelkin/NewCharacterActivity.kt
+++ Androidelkin/app/src/main/java/com/bignerdranch/android/droidelkin/NewCharacterActivity.kt
@@ -3,10 +3,10 @@ package com.bignerdranch.android.droidelkin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NewCharacterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_new_character)
     }
 }
