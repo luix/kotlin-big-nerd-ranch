9754795e8ba333465b6fb26d5b34552aeda90a75
LuiX <luis.a.vivero@gmail.com>
2019-07-14 18:20:50 -0700

    Challenge: Framer Extension

--- NyetHack/src/com/bignerdranch/nyethack/extensions/FrameExtension.kt
+++ NyetHack/src/com/bignerdranch/nyethack/extensions/FrameExtension.kt
@@ -9,9 +9,17 @@ fun frame(name: String, padding: Int, formatChar: String = "*"): String {
     return "$end\n$middle\n$end"
 }
 
+fun String.framer(padding: Int, formatChar: String = "*"): String {
+    val greeting = "$this"
+    val middle = formatChar.padEnd(padding)
+            .plus(greeting)
+            .plus(formatChar.padStart(padding))
+    val doodle = (1 until middle.length).joinToString("") { formatChar }
+    return "$doodle\n$middle\n$doodle"
+}
 
-fun main() {
-    //println("Welcome, Madrigal".frame(5))
+fun main(args: Array<String>) {
+    println("Welcome, Madrigal".framer(5))
 
    /*
     ******************************
