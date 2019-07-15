708d4800429f62fddfd6c1168a02a2afc82da957
LuiX <luis.a.vivero@gmail.com>
2019-07-14 23:05:00 -0700

    Add the CharacterGenerator object

---
+++ Androidelkin/app/src/main/java/com/bignerdranch/android/droidelkin/CharacterGenerator.kt
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.droidelkin
+
+
+private fun <T> List<T>.rand() = shuffled().first()
+
+private fun Int.roll() = (0 until this)
+    .map { (1..6).toList().rand() }
+    .sum()
+    .toString()
+
+private val firstName = listOf("Eli", "Alex", "Sophie")
+private val lastName = listOf("Lightweaver", "Greatfoot", "Oakenfeld")
+
+
+object CharacterGenerator {
+
+    data class CharacterData(val name: String,
+                             val race: String,
+                             val dex: String,
+                             val wis: String,
+                             val str: String)
+
+    private fun name() = "${firstName.rand()} ${lastName.rand()}"
+
+    private fun race() = listOf("dwarf", "elf", "human", "halfing").rand()
+
+    private fun dex() = 4.roll()
+
+    private fun wis() = 3.roll()
+
+    private fun str() = 5.roll()
+
+    fun generate() = CharacterData(
+        name = name(),
+        race = race(),
+        dex = dex(),
+        wis = wis(),
+        str = str()
+    )
+}
