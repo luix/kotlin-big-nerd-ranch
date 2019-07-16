ef6130deeb18c7049192af10102c6372d606db76
LuiX <luis.a.vivero@gmail.com>
2019-07-15 21:46:03 -0700

    Refactoring to displayCharacterData, using Kotlin Android Extension Synthetic Properties.

--- Androidelkin/app/src/main/java/com/bignerdranch/android/droidelkin/NewCharacterActivity.kt
+++ Androidelkin/app/src/main/java/com/bignerdranch/android/droidelkin/NewCharacterActivity.kt
@@ -2,8 +2,7 @@ package com.bignerdranch.android.droidelkin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_new_character.*
 
 class NewCharacterActivity : AppCompatActivity() {
 
@@ -12,13 +11,10 @@ class NewCharacterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_character)
-        val nameTextView = findViewById<TextView>(R.id.nameTextView)
-        val raceTextView = findViewById<TextView>(R.id.raceTextView)
-        val dexterityTextView = findViewById<TextView>(R.id.dexterityTextView)
-        val wisdomTextView = findViewById<TextView>(R.id.wisdomTextView)
-        val strengthTextView = findViewById<TextView>(R.id.strengthTextView)
-        val generateButton = findViewById<Button>(R.id.generateButton)
+        displayCharacterData()
+    }
 
+    fun displayCharacterData() {
         characterData.run {
             nameTextView.text = name
             raceTextView.text = race
