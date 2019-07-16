cf2f7d869aff6c9cd64e28f8de3d152dae59e951
LuiX <luis.a.vivero@gmail.com>
2019-07-15 22:09:02 -0700

    Using the new extension property

--- Androidelkin/app/src/main/java/com/bignerdranch/android/droidelkin/NewCharacterActivity.kt
+++ Androidelkin/app/src/main/java/com/bignerdranch/android/droidelkin/NewCharacterActivity.kt
@@ -16,16 +16,15 @@ class NewCharacterActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putSerializable(CHARACTER_DATA_KEY, characterData)
+        outState.characterData = characterData
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_character)
 
-        characterData = savedInstanceState?.let {
-            it.getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
-        } ?: CharacterGenerator.generate()
+        characterData = savedInstanceState?.characterData ?:
+            CharacterGenerator.generate()
 
         generateButton.setOnClickListener {
             characterData = CharacterGenerator.generate()
