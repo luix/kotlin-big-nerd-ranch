350824b6a8c9f70ba90f81743ed4682fbd6ed8b7
LuiX <luis.a.vivero@gmail.com>
2019-07-15 21:34:52 -0700

    Looking up view elements

--- Androidelkin/app/src/main/java/com/bignerdranch/android/droidelkin/NewCharacterActivity.kt
+++ Androidelkin/app/src/main/java/com/bignerdranch/android/droidelkin/NewCharacterActivity.kt
@@ -2,11 +2,19 @@ package com.bignerdranch.android.droidelkin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class NewCharacterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_character)
+        val nameTextView = findViewById<TextView>(R.id.nameTextView)
+        val raceTextView = findViewById<TextView>(R.id.raceTextView)
+        val dexterityTextView = findViewById<TextView>(R.id.dexterityTextView)
+        val wisdomTextView = findViewById<TextView>(R.id.wisdomTextView)
+        val strengthTextView = findViewById<TextView>(R.id.strengthTextView)
+        val generateButton = findViewById<Button>(R.id.generateButton)
     }
 }
