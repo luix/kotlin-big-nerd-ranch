37890b2e09e0a4be433d683a1e9533a166d7e11e
LuiX <luis.a.vivero@gmail.com>
2020-06-15 21:11:48 -0700

    Add HackerRank's SockMerchant into NyetHack project

--- NyetHack/src/hackerrank/SockMerchant.kt
+++ NyetHack/src/hackerrank/SockMerchant.kt
@@ -1,4 +1,30 @@
 package hackerrank
 
-class SockMerchant {
+import java.util.*
+import kotlin.collections.HashMap
+
+// Complete the sockMerchant function below.
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    val socks = HashMap<Int, Int>()
+    for (n in ar) {
+        val count = socks.get(n) ?: 0
+        socks.put(n, count + 1)
+    }
+    var pairs = 0
+    socks.forEach { k, v ->
+        pairs += (v / 2)
+    }
+    return pairs
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = sockMerchant(n, ar)
+
+    println(result)
 }
