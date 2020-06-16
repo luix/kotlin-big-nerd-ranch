c0fa2833c4fc8cdd0b6c9c5253bf1e3e36cecbf0
LuiX <luis.a.vivero@gmail.com>
2020-06-15 21:12:31 -0700

    Add AnnonymousFunctions into Sandbox project

---
+++ Sandbox/src/AnonymousFunctions.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Hello, world!")
+
+    // Chapter 5. Anonymous Functions and the Function Type
+    val numLetters = "Mississippi".count { it == 's' }
+    println(numLetters)
+}
+
