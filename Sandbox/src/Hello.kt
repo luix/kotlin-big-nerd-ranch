f274490e4d289d26ee26ac2754ffd08b979d4672
LuiX <luis.a.vivero@gmail.com>
2020-06-18 21:06:22 -0700

    Update Hello Kotlin source file in Sandbox project

--- Sandbox/src/Hello.kt
+++ Sandbox/src/Hello.kt
@@ -1,8 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello, world!")
+// https://codelabs.developers.google.com/codelabs/kotlin-bootcamp-functions/#5
 
-    // Chapter 5. Anonymous Functions and the Function Type
-    val numLetters = "Mississippi".count { it == 's' }
-    println(numLetters)
+val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+val lazyMap = decorations.asSequence().map {
+    println("access: $it")
 }
 
+val lazyMap2 = decorations.asSequence().filter { it[0] == 'p' }.map {
+    println("access: $it")
+    it
+}
+
+fun main() {
+
+    // eager, creates a new list
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: " + eager)
+
+    // lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: " + filtered)
+
+    // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: " + newList)
+
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${ lazyMap.first() }")
+    println("-----")
+    println("all: ${ lazyMap.toList() }")
+
+    println("=====")
+    println("filtered: ${ lazyMap2.toList() }")
+}
